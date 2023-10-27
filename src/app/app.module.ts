c8a130da065d8a60f328af519499c864af7b7e43
KuhaiAnna <kuhai.anna@gmail.com>
2023-10-27 04:54:46 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { GeneralInfoComponent } from './general-info/general-info.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ShortInfoComponent } from './short-info/short-info.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, GeneralInfoComponent, ShortInfoComponent, NavigationComponent],
+	imports: [BrowserModule, AppRoutingModule, HttpClientModule, CommonModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
