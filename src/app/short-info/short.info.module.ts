15178a5448061228000f34acb72cca59ec191761
KuhaiAnna <kuhai.anna@gmail.com>
2023-10-27 04:49:45 +0300

    Create short.info.module.ts

---
+++ src/app/short-info/short.info.module.ts
@@ -0,0 +1,10 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { ShortInfoComponent } from './short-info.component';
+
+// @NgModule({
+//   declarations: [ShortInfoComponent],
+//   imports: [CommonModule /* інші імпортовані модулі */],
+//   exports: [ShortInfoComponent],
+// })
+// export class ShortInfoModule {}
