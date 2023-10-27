31842aeebe6e416b68e56f85edfc8907c5916403
KuhaiAnna <kuhai.anna@gmail.com>
2023-10-27 04:54:55 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { GeneralInfoComponent } from './general-info/general-info.component';
+import { ShortInfoComponent } from './short-info/short-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'general-info', component: GeneralInfoComponent },
+	{ path: 'short-info', component: ShortInfoComponent },
+	{ path: '', redirectTo: '/general-info', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
